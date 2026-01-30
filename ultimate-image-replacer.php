af2adf29d4d9fa488ef00d35906ade7e800c3362
Philip Levine <philip@southfloridawebadvisors.com>
2026-01-30 08:59:43 -0500

    Update ultimate-image-replacer.php

--- ultimate-image-replacer.php
+++ ultimate-image-replacer.php
@@ -9,6 +9,7 @@
  * License:     GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: ultimate-image-replacer
+ * Requires PHP: 7.4
  * Tested up to: 6.9
  */
 
